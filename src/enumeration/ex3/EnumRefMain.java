2fc32ec2ff820c5e8be33f6e6ec4933a5108c9e4
seungin <tmddls91@inha.edu>
2025-05-28 02:19:56 +0900

    열거형 - Enum Type

---
+++ src/enumeration/ex3/EnumRefMain.java
@@ -0,0 +1,17 @@
+package enumeration.ex3;
+
+public class EnumRefMain {
+    public static void main(String[] args) {
+        System.out.println("class BASIC = " + Grade.BASIC.getClass());
+        System.out.println("class GOLD = " + Grade.GOLD.getClass());
+        System.out.println("class DIAMOND = " + Grade.DIAMOND.getClass());
+
+        System.out.println("ref BASIC = " + Grade.BASIC);
+        System.out.println("ref GOLD = " + Grade.GOLD);
+        System.out.println("ref DIAMOND = " + Grade.DIAMOND);
+    }
+
+    private static String refValue(Grade grade) {
+        return Integer.toHexString(System.identityHashCode(grade));
+    }
+}
